e23ac99f91ba7658407ce2a3e78ded8b41c38946
neila-a <neila1@yeah.net>
2022-03-27 17:45:55 +0800

    A commit.

--- index.js
+++ index.js
@@ -1,44 +1,17 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-
-var server = http.createServer(function (request, response) {
-    console.log(`${new Date()} Received request for ${request.url}`);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(2319, function () {
-    console.log(`${new Date()} Server is listening on port 2319`);
-});
-
-wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-    return true;
-}
-
-wsServer.on('request', function (request) {
-    if (!originIsAllowed(request.origin)) {
-        request.reject();
-        console.log(`${new Date()} Connection from origin ${request.origin} rejected.`);
-        return;
-    }
-
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log(`${new Date()} Connection accepted.`);
-    connection.on('message', function (message) {
-        if (message.type === 'utf8') {
-            console.log(`Received Message: ${message.utf8Data}`);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log(`Received Binary Message of ${message.binaryData.length} bytes`);
-            connection.sendBytes(message.binaryData);
-        }
+require('http').createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    var params = require('url').parse(req.url, true).query;
+    new require("pusher")({
+        appId: "1367653",
+        key: "68d011b9b45b3e0b44ff",
+        secret: "286d65f732a96f6d460b",
+        cluster: "ap3",
+        useTLS: true
+    }).trigger("files", params.to, {
+        message: params.data
     });
-    connection.on('close', function (reasonCode, description) {
-        console.log(`${new Date()} Peer ${connection.remoteAddress} disconnected.`);
-    });
+    res.write('{ "status": "OK" }');
+    res.end();
+}).listen(80, function () {
+    console.log(`Server listened on port 80.\nClick Ctrl+C to close.`);
 });
