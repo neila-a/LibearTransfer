370ab488f110c45ff26014dd8fdd790060440989
neila-a <neila1@yeah.net>
2022-04-15 19:25:17 +0800

    A commit.

---
+++ index.js
@@ -0,0 +1,37 @@
+var server = new require(
+    "ws"
+).Server(
+    {
+        port: 2319
+    }
+);
+server.on(
+    "connection",
+    function (ws) {
+        console.log("[CLIENT CONNECTIONED]");
+        ws.on(
+            "close",
+            function () {
+                console.log("[CLIENT CLOSED]");
+            }
+        );
+        ws.on(
+            "error",
+            function (err) {
+                console.error(`[CLIENT ERROR]: ${err}`);
+            }
+        );
+        ws.on(
+            "message",
+            function (data) {
+                ws.send(data);
+            }
+        );
+    }
+);
+server.on(
+    "error",
+    function (err) {
+        console.error(`[SERVER ERROR]: ${err}`);
+    }
+);
