c2b2053b849c021b2d52e6d2f7d2bd0da2d42b3b
neila-a <neila1@yeah.net>
2022-04-16 11:01:57 +0800

    A commit.

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const WebSocket = require('ws');
-const WebSocketServer = WebSocket.Server;
-// 创建 websocket 服务器 监听在 2319 端口
-const wss = new WebSocketServer({ port: 2319 });
-console.log(`[${new Date().toString()}] Server created.`);
-// 服务器被客户端连接
-wss.on('connection', function (ws) {
-    console.log(`[${new Date().toString()}] Client connectioned.`);
-    // 接收客户端信息并把信息返回发送
-    ws.on('message', function (message) {
-        ws.send(message, function (err) {
-            if (err) {
-                console.log(`[${new Date().toString()}] Server error: ${err}`);
-            }
+const Pusher = require("pusher");
+const url = reqire("url");
+const http = require("http");
+const push = new Pusher({
+    appId: "1367653",
+    key: "68d011b9b45b3e0b44ff",
+    secret: "286d65f732a96f6d460b",
+    cluster: "ap3",
+    useTLS: true
+});
+var server = http.createServer();
+server.on("request", function (req, res) {
+    try {
+        const params = url.parse(req.url, true).query;
+        push.trigger("my-channel", "my-event", {
+            message: params.msg
         });
-    });
+    } catch {
+        res.end("{ \"status\": \"error\"}");
+        return;
+    }
+    res.end("{ \"status\": \"OK\"}");
+    return;
 });
