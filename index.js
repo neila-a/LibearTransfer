acb0b1e9682df7970bac691be855795e2e187403
neila-a <neila1@yeah.net>
2022-04-16 10:28:25 +0800

    A commit.

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-const cs = require("ws").Server;
-var server = new cs(
-    {
-        port: 2319
-    }
-);
-server.on(
-    "connection",
-    function (ws) {
-        console.log("[CLIENT CONNECTIONED]");
-        ws.on(
-            "close",
-            function () {
-                console.log("[CLIENT CLOSED]");
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+// 创建 websocket 服务器 监听在 2319 端口
+const wss = new WebSocketServer({ port: 2319 });
+console.log(`[${new Date().toString()}] Server created.`);
+// 服务器被客户端连接
+wss.on('connection', function (ws) {
+    console.log(`[${new Date().toString()}] Client connectioned.`);
+    // 接收客户端信息并把信息返回发送
+    ws.on('message', function (message) {
+        ws.send(message, function (err) {
+            if (err) {
+                console.log(`[${new Date().toString()}] Server error: ${err}`);
             }
-        );
-        ws.on(
-            "error",
-            function (err) {
-                console.error(`[CLIENT ERROR]: ${err}`);
-            }
-        );
-        ws.on(
-            "message",
-            function (data) {
-                ws.send(data);
-            }
-        );
-    }
-);
-server.on(
-    "error",
-    function (err) {
-        console.error(`[SERVER ERROR]: ${err}`);
-    }
-);
-console.log("[SERVER OPENED]");
+        });
+    });
+});
