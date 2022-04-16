e26dec974f9d46ac7e8bda4ba384cb3fd4a27353
neila-a <neila1@yeah.net>
2022-04-16 11:19:33 +0800

    1

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-const Pusher = require("pusher");
-const url = require("url");
-const http = require("http");
-const push = new Pusher({
-    appId: "1367653",
-    key: "68d011b9b45b3e0b44ff",
-    secret: "286d65f732a96f6d460b",
-    cluster: "ap3",
-    useTLS: true
-});
-var server = http.createServer();
-server.on("request", function (req, res) {
-    try {
-        const params = url.parse(req.url, true).query;
-        push.trigger("my-channel", "my-event", {
-            message: params.msg
-        });
-    } catch {
-        res.end("{ \"status\": \"error\"}");
-        return;
+const cs = require("ws").Server;
+var server = new cs(
+    {
+        port: 2319
     }
-    res.end("{ \"status\": \"OK\"}");
-    return;
-});
-server.listen(80);
+);
+server.on(
+    "connection",
+    function (ws) {
+        console.log("[CLIENT CONNECTIONED]");
+        ws.on(
+            "close",
+            function () {
+                console.log("[CLIENT CLOSED]");
+            }
+        );
+        ws.on(
+            "error",
+            function (err) {
+                console.error(`[CLIENT ERROR]: ${err}`);
+            }
+        );
+        ws.on(
+            "message",
+            function (data) {
+                ws.send(data);
+            }
+        );
+    }
+);
+server.on(
+    "error",
+    function (err) {
+        console.error(`[SERVER ERROR]: ${err}`);
+    }
+);
+console.log("[SERVER OPENED]");
+>>>>>>> parent of acb0b1e (A commit.)
