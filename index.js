c92e1dfd3c2b5a942a2e3e1e6474bddea1ef7558
neila-a <neila1@yeah.net>
2022-04-16 13:37:26 +0800

    A commit.

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
-const cs = require("ws").Server;
-var port = process.env.PORT || 2319;
-console.log(port);
-var server = new cs(
-    {
-        port: port
-    }
-);
-server.on(
-    "connection",
-    function (ws) {
-        console.log("[CLIENT CONNECTIONED]");
-        ws.on(
-            "close",
-            function () {
-                console.log("[CLIENT CLOSED]");
+const express = require("express");
+const { Server } = require('ws');
+const PORT = process.env.PORT || 80;
+const server = express().use(function (req, res) {
+    return res.sendFile(`/static${req.url.replace(/\?.*/, "")}`, { root: __dirname });
+}).listen(PORT, function () {
+    return console.log(`Listening on ${PORT}`);
+});
+const wss = new Server({ server });
+wss.on('connection', function (ws) {
+    console.log('Client connected');
+    ws.on('close', function () {
+        return console.log('Client disconnected');
+    });
+    ws.on('message', function (message) {
+        ws.send(message, function (err) {
+            if (err) {
+                console.log(`[SERVER] error: ${err}`);
             }
-        );
-        ws.on(
-            "error",
-            function (err) {
-                console.error(`[CLIENT ERROR]: ${err}`);
-            }
-        );
-        ws.on(
-            "message",
-            function (data) {
-                ws.send(data);
-            }
-        );
-    }
-);
-server.on(
-    "error",
-    function (err) {
-        console.error(`[SERVER ERROR]: ${err}`);
-    }
-);
-console.log("[SERVER OPENED]");
+        });
+    });
+});
