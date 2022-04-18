6a4a69ebd701238e9653a1b3269924a104912c1e
neila-a <neila1@yeah.net>
2022-04-18 21:23:22 +0800

    A commit.

--- index.js
+++ index.js
@@ -1,62 +1,70 @@
 const express = require("express");
+const fs = require('fs');
 const { Server } = require('ws');
-const PORT = process.env.PORT || 80;
-const server = express().use(function (req, res) {
-    return res.sendFile(`/static${req.url.replace(/\?.*/, "")}`, { root: __dirname });
-}).listen(PORT, function () {
-    return console.log(`Listening on port ${PORT}.`);
-});
+const peer = require('peer');
 const wss = new Server({ server });
+var server = peer.PeerServer({
+    port: process.env.PORT || 80,
+    ssl: {
+        cert: fs.readFileSync('key.crt')
+    },
+    path: "/peer/"
+});
+/*const server = express().use(function (req, res) {
+    return res.sendFile(`/static${req.url.replace(/\?.*/      /*, "")}`, { root: __dirname });
+}).listen(PORT, function () {
+return console.log(`Listening on port ${PORT}.`);
+});
 wss.on('connection', function (ws) {
-    console.log('[CLIENT] Connected.');
-    ws.on('close', function () {
-        return console.log('[CLIENT] Disconnected.');
+console.log('[CLIENT] Connected.');
+ws.on('close', function () {
+    return console.log('[CLIENT] Disconnected.');
+});
+ws.on('message', function (message) {
+    const msg = message.toString();
+    console.log("[CLIENT] Sended message.");
+    ws.send(msg, function (err) {
+        if (err) {
+            console.log(`[SERVER] Error: ${err}`);
+        }
     });
-    ws.on('message', function (message) {
-        const msg = message.toString();
-        console.log("[CLIENT] Sended message.");
-        ws.send(msg, function (err) {
-            if (err) {
-                console.log(`[SERVER] Error: ${err}`);
-            }
-        });
-        console.log(wss.clients);
-    });
+    console.log(wss.clients);
+});
 });
 function on_server_headers(data) {
-    // console.log(data);
+// console.log(data);
 }
 wss.on("headers", on_server_headers);
 
-/*
+
 function websocket_add_listener(client_sock) {
-    // close事件
-    client_sock.on("close", function () {
-        console.log("client close");
-    });
-    client_sock.on("error", function (err) {
-        console.log("client error", err);
-    });
-    client_sock.on("message", function (data) {
-        console.log(data);
-        client_sock.send(data.toString());
-    });
+// close事件
+client_sock.on("close", function () {
+    console.log("client close");
+});
+client_sock.on("error", function (err) {
+    console.log("client error", err);
+});
+client_sock.on("message", function (data) {
+    console.log(data);
+    client_sock.send(data.toString());
+});
 }
 function on_server_client_comming(client_sock) {
-    console.log("client comming");
-    websocket_add_listener(client_sock);
+console.log("client comming");
+websocket_add_listener(client_sock);
 }
 
 wss.on("connection", on_server_client_comming);
 
 // error事件,表示的我们监听错误;
 function on_server_listen_error(err) {
-    console.log(err)
+console.log(err)
 }
 wss.on("error", on_server_listen_error);
 
 // headers事件, 回给客户端的字符。
 function on_server_headers(data) {
-    console.log("data:", data);
+console.log("data:", data);
 }
 wss.on("headers", on_server_headers);*/
