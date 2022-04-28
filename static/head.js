e68b043b17632a4768ed4ac827acb2f2986c919e
neila-a <neila1@yeah.net>
2022-04-28 21:39:56 +0800

    A commit.

--- static/head.js
+++ static/head.js
@@ -214,17 +214,20 @@ const showabout = function () {
     `);
     initmount();
 };
-Notification.requestPermission().then(function (permission) {
-    // 如果用户接受权限，我们就可以发起一条消息
-    if (permission === "granted") {
-        var notification = new Notification("Hello", {
-            dir: "auto",
-            lang: "zh-CN",
-            tag: "msg",
-            icon: "./libear-only.png"
-        });
-    }
-});
+const noti = function (t, b) {
+    Notification.requestPermission().then(function (permission) {
+        // 如果用户接受权限，我们就可以发起一条消息
+        if (permission === "granted") {
+            var notification = new Notification(t, {
+                dir: "auto",
+                lang: "zh-CN",
+                tag: "msg",
+                icon: "./libear-only.png",
+                body: b
+            });
+        }
+    });
+};
 if (localStorage.theme_color == undefined) localStorage.setItem("theme_color", "#1e9fff");
 const showtheme = function () {
     window.paletteonchange = function (event) {
