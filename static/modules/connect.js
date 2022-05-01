751453305c445157d23a8fa4ace1e810e532a011
neila-a <neila1@yeah.net>
2022-05-01 17:25:34 +0800

    A commit.

--- static/modules/connect.js
+++ static/modules/connect.js
@@ -1,3 +1,8 @@
+const peerconn = new Peer(window.id, {
+    host: location.hostname,
+    debug: 1,
+    path: '/peer'
+});
 peerconn.on('open', function () {
 });
 peerconn.on('connection', function (conn) {
--- static/modules/id.js
+++ static/modules/id.js
@@ -6,11 +6,6 @@ fpPromise.then(function (fp) {
 }).then(function (result) {
     const nid = result.visitorId;
     window.id = nid.substring(nid.length - 9);
-    window.peerconn = new Peer(id, {
-        host: location.hostname,
-        debug: 1,
-        path: '/peer'
-    });
     document.head.innerHTML = `${document.head.innerHTML}<script src="./modules/connect.js"></script>`;
 }).catch(function (error) {
     return console.error(error);
